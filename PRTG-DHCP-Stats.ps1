5d4534814c41062c82694050bdbcab0a566beb84
Jannos-443 <77551365+Jannos-443@users.noreply.github.com>
2021-03-12 10:56:39 +0100

    Update PRTG-DHCP-Stats.ps1

--- PRTG-DHCP-Stats.ps1
+++ PRTG-DHCP-Stats.ps1
@@ -1,4 +1,4 @@
-﻿<#
+<#
     .SYNOPSIS
     Monitors DHCP Scopes (PercentageInUse, AddressesFree and AddressesInUse)
     Monitors DHCP Failover State and Mode
@@ -43,7 +43,11 @@
 #>
 param(
 [Parameter(Mandatory)] [string]$DHCPServer = $null,
-[switch]$CheckFailOver = $true,
+[switch]$PercentageInUse = $true,
+[switch]$CheckFailOver = $false,
+[switch]$AddressesFree = $false,
+[switch]$AddressesInUse = $false,
+[switch]$ReservedAddress = $false,
 [string]$IgnorePattern = ""
 )
 
@@ -80,7 +84,8 @@ $xmlOutput = '<prtg>'
 #Check Scope state for each Scope
 foreach ($scope in $dhcpScopeStats)
 {
-    $xmlOutput = $xmlOutput + "<result>
+    if($PercentageInUse){
+        $xmlOutput = $xmlOutput + "<result>
         <channel>Scope: $($scope.ScopeId) -PercentageInUse</channel>
         <value>$([math]::Round($scope.PercentageInUse))</value>
         <unit>Percent</unit>
@@ -89,8 +94,10 @@ foreach ($scope in $dhcpScopeStats)
         <LimitMaxWarning>90</LimitMaxWarning>
         <LimitErrorMsg>DHCP Scope is over 95%</LimitErrorMsg>
         <LimitWarningMsg>DHCP Scope is over 90%</LimitWarningMsg>
-    </result>
-    <result>
+        </result>"
+    }
+    if($AddressesFree){
+        $xmlOutput = $xmlOutput + "<result>
         <channel>Scope: $($scope.ScopeId)  -AddressesFree</channel>
         <unit>Custom</unit>
         <customUnit>IP</customUnit>
@@ -99,8 +106,10 @@ foreach ($scope in $dhcpScopeStats)
         <showTable>1</showTable>
         <float>0</float>
         <value>  $($scope.AddressesFree) </value>                           
-    </result>
-    <result>
+        </result>"
+    }
+    if($AddressesInUse){
+        $xmlOutput = $xmlOutput + "<result>
         <channel>Scope:  $($scope.ScopeId)  -AddressesInUse</channel>
         <unit>Custom</unit>
         <customUnit>IP</customUnit>
@@ -109,8 +118,20 @@ foreach ($scope in $dhcpScopeStats)
         <showTable>1</showTable>
         <float>0</float>
         <value>  $($scope.AddressesInUse) </value>
-    </result>
-"
+        </result>"
+    }
+    if($ReservedAddress){
+        $xmlOutput = $xmlOutput + "<result>
+        <channel>Scope: $($scope.ScopeId)  -ReservedAddress</channel>
+        <unit>Custom</unit>
+        <customUnit>IP</customUnit>
+        <mode>Absolute</mode
+        ><showChart>1</showChart>
+        <showTable>1</showTable>
+        <float>0</float>
+        <value> $($scope.ReservedAddress) </value>
+        </result>"
+    }
     
 }
 
